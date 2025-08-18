dcf854d64152e2d7ac3ba4a17f8b3eed68a54f20
ACN <ACN@Paloma.ikn.psy.ruhr-uni-bochum.de>
2025-08-18 14:38:59 +0200

    defaults improved

--- set_default.m
+++ set_default.m
@@ -81,19 +81,8 @@ if isfield(opt,'kilosort')
        addpath(genpath(input.KSpath)) % path to kilosort toolbox
     
     elseif opt.kilosort == 4
-       % % input.KSpython = 'C:\Code\miniconda3\envs\kilosort\'; 
-       % %  input.KSpyfolder = [input.KSpython,'\Lib\site-packages\kilosort']; % Path to the kilosort git-code. It could change among PCs
-       % % %input.KSpyenv_NGL = 'C:\Code\miniconda3\envs\kilosort\Lib\site-packages\kilosort'; %'C:\Users\ballejd4\AppData\Local\miniconda3\envs\kilosort'; % Kilosort enviroment under python installation. It could change among PCs
-% % Added 15.08.2025 for testing ...
-       input.KSpython = 'C:\Users\ACN\anaconda3\envs\kilosort\'; % where the desired python.exe version
-        input.KSpyfolder = [input.KSpython,'\Lib\site-packages\kilosort']; % Path to the kilosort enviroment using that python version
-% % ... till here
-
-       % Copy NGL customized py files to kilosort enviroment's library
-       orig_dir = pwd; % where we come from
-       cd(input.analysisCode) % go to where customized py files are, \analysisCode
-            copyfile("*.py", input.KSpyfolder); % copy the customized py files to the working directory
-       cd(orig_dir) % back to previous folder
+       input.KSpython = 'C:\Code\miniconda3\envs\kilosort\'; 
+        input.KSpyfolder = [input.KSpython,'\Lib\site-packages\kilosort']; % Path to the kilosort git-code. It could change among PCs
     
        % No need to add to matlab path
     end
