1b707a8cd1f4bc160d65d157219f9d0427343923
ACN <ACN@Sparkler>
2025-11-06 14:23:12 +0100

    Simplified

--- NGL_SetAndRunMe.m
+++ NGL_SetAndRunMe.m
@@ -20,8 +20,6 @@ readmecontent = ["Study name: DefaultName", ...
 % RECOMMENDED to add the toolbox folder to MATLAB folder system, but not necessary.
 datadrive   = 'E';                   % The LETTER of the drive where the data structure is/will be created.
 studyname   = 'ProjectName';  % Name of the study to be used (main folder for the data)
-toolbox     = 'C:\Code\ephys-data-pipeline'; % Absolute path to the toolbox.
-pythonExe   = 'C:\Code\miniconda3\envs\kilosort\'; % Absolute path to the KS python executable under KS enviroment
 
 % before any data exists, since the folder for the raw data is created
 % here. If it already exists, nothing will change.
@@ -38,19 +36,19 @@ opt = struct();
     % General options for NGL01_Main
     opt.numChannels         = 32;       % For now, explicit 32 if not SpikeLog-64C was used (Deuteron). INTAN: comment.
     opt.noise               = [];
-    opt.CAR                 = 1;        % Default: 1. CAR to remove fast-ample transients and other noise for .bin file. If == 2 also CAR for lowpass (not recommended)
+    opt.CAR                 = 0;        % Default: 1. CAR to remove fast-ample transients and other noise for .bin file. If == 2 also CAR for lowpass (not recommended)
     opt.linefilter          = 0;        % If not 0, filter line noise at given value +-2 (Hz)
     opt.doNWB               = false;     % TESTING INTAN-NEUROCONV (python) with a Matlab wrapping for no python-user interaction
     opt.bin                 = true;     % Create a .bin file with the high-pass data, to be passed to Kilosort for spike sorting.
-        opt.highpass        = 400;      % Give as low boundary frequency value. (High boundary is set at recording time)
+        opt.highpass        = 0;      % Give as low boundary frequency value. (High boundary is set at recording time)
     opt.FieldTrip           = true;     % Create a FieldTrip ready .mat file with the low-pass data, either continuous, trial-parsed or both. 
         opt.lowpass         = 250;      % Give as high boundary frequency value.
     opt.GetMotionSensors    = true;    % Retrieve data from motion sensors in Deuteron. NEEDS IMPROVEMENT on head direction interpretation.
     opt.RetrieveEvents      = true;     % Retrieve event log. If not further options defaulted to Deuteron txt log extraction.
-        opt.alignto         = {'itiOn', 'stimOn1', 'stimOn2', 'rwd'};  % single char array e.g. 'itiOn', or cell array e.g. {'itiOn', 'rwd'}. 'itiON' should be the very least to align to.
+        opt.alignto         = {'itiOn', 'stimOn1', 'rwd'};  % single char array e.g. 'itiOn', or cell array e.g. {'itiOn', 'rwd'}. 'itiON' should be the very least to align to.
         opt.trEvents        = {'tr1', 'tr2'}; % Event definition of 'special events' i.e. events at the ITI like treatments, tutors, etc...
-        opt.addtime         = 1500;     % Expands the trial definition start/end by X ms in both directions. 
-    opt.KSchanMapFile       = 'chanMap_ATLAS_E32-S2+NN_A1x32-Poly3_INTAN.mat';  % Empty '' to use non-mapped, linear array. Or e.g.'chanMapXXX.mat' for custom maps saved under 'studyName\analysisCode\'
+        opt.addtime         = 1000;     % Expands the trial definition start/end by X ms in both directions. 
+    opt.KSchanMapFile       = 'chanMap_XXX.mat';  % Empty '' to use non-mapped, linear array. Or e.g.'chanMapXXX.mat' for custom maps saved under 'studyName\analysisCode\'
     opt.bombcell            = true;    % Run bombcell on the KS output. =2 (KS2) or =4 (KS4). Previous step to manual curation.
     opt.phy                 = false;    % Open phy for manual inspection or curation. !! It puts MATLAB on HOLD! Needs bin file in same folder.
                         
