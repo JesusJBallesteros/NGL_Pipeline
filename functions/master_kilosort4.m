b70d8ccdc38916bb69e529e99b8421ddf43bf97e
unknown <ACN@Sparkler.ruhr-uni-bochum.de>
2025-11-06 16:28:38 +0100

    Highpass filtering delegated to KS by default (recommended)

--- functions/master_kilosort4.m
+++ functions/master_kilosort4.m
@@ -111,7 +111,7 @@ clear content
 %% Set up Kilosort enviroment
 % Call enviroment status
 %pe = pyenv;
-pe = pyenv(Version=[input.pythonExe,'python.exe'], ExecutionMode="OutOfProcess");
+pe = pyenv(Version=[input.KSpythonExe,'python.exe'], ExecutionMode="OutOfProcess");
 
 % Check if pyenv is set, or kill any residual process running
 if pe.ExecutionMode && pe.Status > 0
