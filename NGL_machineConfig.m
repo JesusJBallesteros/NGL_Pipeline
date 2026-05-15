dc4b89101d73de2ac46de4f0abe416be509b2265
Jesus Ballesteros <jesusjballesteros@gmail.com>
2026-05-15 11:07:55 +0200

    Removing NGL_machineConfig from gitignore, so it is easier to find and customize when necessary.

---
+++ NGL_machineConfig.m
@@ -0,0 +1,20 @@
+function cfg = NGL_machineConfig()
+    % These are the only values that should differ between lab machines.
+    % Each user maintains a local copy. 
+    %
+    % The values in the file stored within the pipeline code should work in
+    % any of the Workstations at NBL lab.
+
+    % Where the pipeline code is stored:
+    cfg.toolbox      = 'C:\Code\ephys-data-pipeline';
+
+    % Where the 'kilosort' environment has been created 
+    cfg.KSpythonExe  = 'C:\Users\ballejd4\AppData\Local\miniconda3\envs\kilosort\';
+
+    % Where the 'phy' environment has been created
+    cfg.PHYpythonExe = 'C:\Users\ballejd4\AppData\Local\miniconda3\envs\phy2\';
+
+    % The exe file used within the 'neuroconv' environment
+    cfg.NCpythonExe  = 'C:\Users\ballejd4\AppData\Local\miniconda3\envs\neuroconv\';
+
+end
